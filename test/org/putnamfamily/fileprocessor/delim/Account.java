1947afab363dd607b5c23da4b8adb24529d8c735
David Putnam <davidtputnam@hotmail.com>
2023-08-09 22:20:27 -0500

    Add deliminated file example

---
+++ test/org/putnamfamily/fileprocessor/delim/Account.java
@@ -0,0 +1,49 @@
+
+package org.putnamfamily.fileprocessor.delim;
+
+public class Account {
+    private String registration1;
+    private String registration2;
+    private String registration3;
+    /**
+     * @return the registration1
+     */
+    public String getRegistration1() {
+        return registration1;
+    }
+
+    /**
+     * @param registration1 the registration1 to set
+     */
+    public void setRegistration1(String registration1) {
+        this.registration1 = registration1;
+    }
+
+    /**
+     * @return the registration2
+     */
+    public String getRegistration2() {
+        return registration2;
+    }
+
+    /**
+     * @param registration2 the registration2 to set
+     */
+    public void setRegistration2(String registration2) {
+        this.registration2 = registration2;
+    }
+
+    /**
+     * @return the registration3
+     */
+    public String getRegistration3() {
+        return registration3;
+    }
+
+    /**
+     * @param registration3 the registration3 to set
+     */
+    public void setRegistration3(String registration3) {
+        this.registration3 = registration3;
+    }
+}
